a33630a4abe3af58b68801af373305ea828811c8
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-14 17:10:12 +0200

    first command and debug cmdlet

---
+++ ProductivityTools.PSDisplayPosition.Cmdlet/Commands/MoveDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.PSDisplayPosition.Cmdlet.Commands
+{
+    public class MoveDisplay : PSCmdlet.PSCommandPT<ExternalMonitorToLeftCmdlet>
+    {
+        public MoveDisplay(ExternalMonitorToLeftCmdlet cmdletType) : base(cmdletType)
+        {
+        }
+
+        protected override bool Condition => true;
+
+        protected override void Invoke()
+        {
+            WriteOutput("Hello");
+        }
+    }
+}
---
+++ ProductivityTools.PSDisplayPosition.Cmdlet/ExternalMonitorToLeftCmdlet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using ProductivityTools.PSCmdlet;
+using ProductivityTools.PSDisplayPosition.Cmdlet.Commands;
+
+namespace ProductivityTools.PSDisplayPosition.Cmdlet
+{
+    [Cmdlet(VerbsCommon.Move,"ExternalMonitorToLeft")]
+    public class ExternalMonitorToLeftCmdlet : PSCmdletPT
+    {
+        protected override void BeginProcessing()
+        {
+            base.AddCommand(new MoveDisplay(this));
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            this.ProcessCommands();
+        }
+    }
+}
